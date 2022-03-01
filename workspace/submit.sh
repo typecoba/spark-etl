6ab0968a47979a36dd294d4ebc02f0e80d9d0217
typecoba <typecoba@gmail.com>
2022-03-02 05:58:59 +0900

    feat: spark submit 조건수정

--- workspace/submit.sh
+++ workspace/submit.sh
@@ -1,10 +1,10 @@
 spark-submit \
     --master local[6] \
     --num-executors 2 \
-    --executor-memory 2G \
+    --executor-memory 12G \
     --executor-cores 2 \
     --conf spark.yarn.am.waitTime=900000 \
     --conf spark.core.connection.ack.wait.timeout=600s \
     --conf spark.dynamicAllocation.enabled=false \
-    --conf spark.default.parallelism=4 \
-    ./app.py 
+    --conf spark.default.parallelism=6 \
+    ./app.py
