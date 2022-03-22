9ad9a35ef4c99a68f648c4883280385a58ebbac0
typecoba <typecoba@gmail.com>
2022-03-22 13:24:25 +0900

    feat: app & submit 관련 실행옵션 수정

--- workspace/app.py
+++ workspace/app.py
@@ -1,5 +1,7 @@
 import sys
 from process.tier1.EventStandardization import EventStandardization
+from process.tier2.PurchaseByUser import PurchaseByUser
+from process.tier2.EventByUser import EventByUser
 
 '''
 arg[0] : 프로세스 종류
@@ -7,8 +9,15 @@ arg[0] : 프로세스 종류
 
 if __name__=='__main__':
     # 매개변수 받아 process 선택
-    print(sys.argv)
-    # if sys.argv==None or sys.argv[0] == 'preprocess':
-    
-    EventStandardization = EventStandardization()
-    EventStandardization.run()
+    # print(sys.argv)
+    if sys.argv==None:
+        pass
+    elif sys.argv[1] == 'eventStandardization':
+        eventStandardization = EventStandardization()
+        eventStandardization.run()
+    elif sys.argv[1] == 'purchaseByUser':
+        purchaseByUser = PurchaseByUser()
+        purchaseByUser.run()
+    elif sys.argv[1] == 'eventByUser':
+        eventByUser = EventByUser()
+        eventByUser.run()
--- workspace/submit.sh
+++ workspace/submit.sh
@@ -1,10 +1,10 @@
 spark-submit \
     --master local[6] \
     --num-executors 2 \
-    --executor-memory 12G \
+    --executor-memory 8G \
     --executor-cores 2 \
     --conf spark.yarn.am.waitTime=900000 \
     --conf spark.core.connection.ack.wait.timeout=600s \
     --conf spark.dynamicAllocation.enabled=false \
     --conf spark.default.parallelism=6 \
-    ./app.py
+    ./app.py eventByUser
